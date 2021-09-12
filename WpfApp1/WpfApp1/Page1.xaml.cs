f168c9857f220f4312967e24e238a2b5e7cc673c
iluy420 <74922816+iluy420@users.noreply.github.com>
2021-09-12 21:26:28 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/Page1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public class gg
+    {
+        public static int coun { get; set; } = 0;
+        public int count  { get; set; } = coun;
+        public gg()
+        {
+            coun++;
+        }
+        public gg(int a)
+        {
+            
+        }
+
+    }
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(opoi.IsChecked == true)
+            {
+                gg h = new gg();
+            }
+            NavigationService.Navigate(new Page2());
+        }
+
+       
+    }
+}
---
+++ WpfApp1/WpfApp1/Page3.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            gg h = new gg(1);
+
+            if (h.count < 2)
+            {
+                opoi3.Text = "у тебя параша черт";
+
+            }
+            else
+            {
+                opoi3.Text = "5 иди на хуй";
+            }
+          
+        }
+    }
+}
